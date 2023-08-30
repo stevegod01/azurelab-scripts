b85ab84ac7e9f05bf0e05b4e93aec06c030d2330
Emeka Ejiogu <52792537+stevegod01@users.noreply.github.com>
2023-08-30 11:45:39 +0100

    Update jumpbox.sh

--- Azure Load Balancer/jumpbox.sh
+++ Azure Load Balancer/jumpbox.sh
@@ -7,13 +7,13 @@ do
 j=$(($i + 4))
 ip="10.0.2.$j"
 sshpass -p "VMP@55w0rd" \
-ssh -o StrictHostKeyChecking=no kodekloud@$ip bash -c  \
+ssh -o StrictHostKeyChecking=no azureuser@$ip bash -c  \
 "'export VAR=$i
 printenv | grep VAR
 echo "Setting up webserver-0$i VM"
 sudo apt install apache2 -y
 sudo chmod -R -v 777 /var/www/
-sudo curl "https://raw.githubusercontent.com/rithinskaria/kodekloud-azure/main/Azure%20Load%20Balancer/sample.html" > /var/www/html/index.html
+sudo curl "https://github.com/stevegod01/azurelab-scripts/blob/main/Azure%20Load%20Balancer/sample.html" > /var/www/html/index.html
 sed -i "s/PAGECOLOR/${colors[$i]}/g" /var/www/html/index.html
 exit
 '"
